f17975715ef66e9c6f311e6ead7876ae6ed4c27b
mick.yi <mick.yi@embracesource.com>
2019-01-31 09:46:34 +0800

    增加rpn to proposals 层

--- faster_rcnn/layers/target.py
+++ faster_rcnn/layers/target.py
@@ -155,7 +155,10 @@ def rpn_targets_graph(gt_boxes, gt_cls, anchors, rpn_train_anchors=None):
     gt_num = tf.shape(gt_cls)[0]  # GT数
     miss_match_gt_num = gt_num - tf.shape(tf.unique(positive_gt_indices)[0])[0]  # 未分配anchor的GT
 
-    return class_ids, deltas, indices, gt_num, positive_num, miss_match_gt_num
+    return class_ids, deltas, indices, tf.cast(gt_num, dtype=tf.float32), tf.cast(positive_num,
+                                                                                  dtype=tf.float32), tf.cast(
+        miss_match_gt_num, dtype=tf.float32)
+    # positive_num, miss_match_gt_num
 
 
 class RpnTarget(keras.layers.Layer):
@@ -198,6 +201,107 @@ class RpnTarget(keras.layers.Layer):
                 (input_shape[0][0],),
                 (input_shape[0][0],),
                 (input_shape[0][0],)]
+
+
+def apply_regress(deltas, anchors):
+    """
+    应用回归目标到边框
+    :param deltas: 回归目标[N,(dy, dx, dh, dw)]
+    :param anchors: anchor boxes[N,(y1,x1,y2,x2)]
+    :return:
+    """
+    # 高度和宽度
+    h = anchors[:, 2] - anchors[:, 0]
+    w = anchors[:, 3] - anchors[:, 1]
+
+    # 中心点坐标
+    cy = (anchors[:, 2] + anchors[:, 0]) * 2
+    cx = (anchors[:, 3] + anchors[:, 1]) * 2
+
+    # 回归系数
+    deltas *= tf.constant([0.1, 0.1, 0.2, 0.2])
+    dy, dx, dh, dw = deltas[:, 0], deltas[:, 1], deltas[:, 2], deltas[:, 3]
+
+    # 中心坐标回归
+    cy += dy * h
+    cx += dx * w
+    # 高度和宽度回归
+    h *= tf.exp(dh)
+    w *= tf.exp(dw)
+
+    # 转为y1,x1,y2,x2
+    y1 = cy - h * 0.5
+    x1 = cx - w * 0.5
+    y2 = cy + h * 0.5
+    x2 = cx + w * 0.5
+
+    return tf.stack([y1, x1, y2, x2], axis=1)
+
+
+def nms(boxes, scores, max_output_size, iou_threshold=0.5, score_threshold=0.05, name=None):
+    """
+    非极大抑制
+    :param boxes:
+    :param scores:
+    :param max_output_size:
+    :param iou_threshold:
+    :param score_threshold:
+    :param name:
+    :return: 检测边框、边框得分
+    """
+    indices = tf.image.non_max_suppression(boxes, scores, max_output_size, iou_threshold, score_threshold, name)  # 一维索引
+    output_boxes = tf.gather(boxes, indices)  # (M,4)
+    class_scores = tf.gather(scores, indices)
+    # padding到固定大小
+    return tf_utils.pad_to_fixed_size(output_boxes, max_output_size), \
+           tf_utils.pad_to_fixed_size(class_scores, max_output_size)
+
+
+class RpnToProposal(keras.layers.Layer):
+    """
+    生成候选框
+    """
+
+    def __init__(self, batch_size, score_threshold=0.05, output_box_num=300, iou_threshold=0.5):
+        """
+
+        :param batch_size: batch_size
+        :param score_threshold: 分数阈值
+        :param output_box_num: 生成proposal 边框数量
+        :param iou_threshold: nms iou阈值
+        """
+        self.batch_size = batch_size
+        self.score_threshold = score_threshold
+        self.max_box_num = output_box_num
+        self.iou_threshold = iou_threshold
+
+    def call(self, inputs, **kwargs):
+        """
+        应用边框回归，并使用nms生成最后的边框
+        :param inputs:
+        inputs[0]: deltas, [N,(dy,dx,dh,dw)]
+        inputs[1]: class logits [N,num_classes]
+        inputs[2]: anchors [N,(y1,x1,y2,x2)]
+        :param kwargs:
+        :return:
+        """
+        deltas = inputs[0]
+        class_logits = inputs[1]
+        anchors = inputs[2]
+        # 转为分类评分
+        class_scores = tf.nn.softmax(logits=class_logits, axis=-1)[:, -1]  # 最后一类为前景 (N,)
+
+        # 应用边框回归
+        proposals = tf_utils.batch_slice([deltas, anchors], lambda x, y: apply_regress(x, y), self.batch_size)
+
+        # 非极大抑制
+        output_boxes = tf_utils.batch_slice([proposals, class_scores],
+                                            lambda x, y: nms(x, y,
+                                                             max_output_size=self.output_box_num,
+                                                             iou_threshold=self.iou_threshold,
+                                                             score_threshold=self.score_threshold),
+                                            self.batch_size)
+        return output_boxes
 
 
 if __name__ == '__main__':
