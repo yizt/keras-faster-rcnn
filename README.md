4e2597ca5f585ab45789924bf0002d9883b898a0
mick.yi <mick.yi@embracesource.com>
2019-03-05 11:08:05 +0800

    完成评估过程

--- README.md
+++ README.md
@@ -43,26 +43,46 @@ voc_path = '/opt/dataset/VOCdevkit'
 
 
 
-e) 训练rpn网络; 训练日志见[train.rpn.log](train.rpn.log)
+e) 训练rpn网络; 训练日志见[train.rpn.log](train.rpn.log)  (optional)
 
 ```shell
-python train.py
+python train.py --stages rpn
 ```
 
 f) end2end 方式联合训练rpn和rcnn ;  训练日志见[train.rcnn.log](train.rcnn.log) ; 
 
 ```shell
-python train.py --stages rcnn
+python train.py
 ```
 
 注：也可以直接联合训练rpn和rcnn；不用训练rpn这一步; 如果没有预先训练rpn;训练时需要训练更多轮。
 
-## 预测
+## 评估
 
-在jupyter notebook中执行如下命令;详见: [测试.ipynb](测试.ipynb) ;
+执行如下命令即可
+
+```shell
+python evaluate.py
+```
+
+输出结果如下：
 
 ```
-!python inference.py
+ap:{0: 0.0, 1: 0.5909308613134401, 2: 0.5195146361895532, 3: 0.513946822748238, 4: 0.490973073476093, 5: 0.44759731449012313, 6: 0.43333154556028486, 7: 0.33117353141276584, 8: 0.19636205120076086, 9: 0.3748558681355139, 10: 0.29942392861354306, 11: 0.4040434302434174, 12: 0.611310224080139, 13: 0.4257329355729211, 14: 0.09090909090909091, 15: 0.5597529101905939, 16: 0.33091240504469827, 17: 0.15495867768595042, 18: 0.22335361205793103, 19: 0.5080513656747481, 20: 0.42761708700467765}
+mAP:0.39673756858022424
+
+```
+
+
+
+
+
+## 预测
+
+执行如下命令即可
+
+```shell
+python inference.py
 ```
 
 样例结果如下;更多预测样例见demo_images目录
@@ -71,9 +91,10 @@ python train.py --stages rcnn
 
 
 ## todo
-0. 评估标准
+0. 评估标准(已完成)
 1. 边框裁剪放到Anchors层外面
 2. batch_slice部分重构
 3. 模型编译重构,分离度量指标
 4. GT boxes信息加载部分重构
 5. 不同输入尺寸大小精度比较
+6. indices类型改为tf.int64;float32类型转换可能丢失精度
--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,64 @@
 # -*- coding: utf-8 -*-
 """
    File Name：     evaluate
-   Description :
+   Description :   frcnn评估
    Author :       mick.yi
    date：          2019/3/4
 """
+import numpy as np
+from faster_rcnn.preprocess.input import VocDataset
+from faster_rcnn.config import current_config as config
+from faster_rcnn.utils import np_utils, image as image_utils, eval_utils
+from faster_rcnn.layers import models
+
+
+def main():
+    # 加载数据集
+    dataset = VocDataset(config.voc_path, class_mapping=config.CLASS_MAPPING)
+    dataset.prepare()
+    print("len:{}".format(len(dataset.get_image_info_list())))
+    test_image_info_list = [info for info in dataset.get_image_info_list() if info['type'] == 'test']
+    print("len:{}".format(len(test_image_info_list)))
+    # 加载模型
+    m = models.frcnn((config.IMAGE_MAX_DIM, config.IMAGE_MAX_DIM, 3), 1, config.NUM_CLASSES,
+                     50, config.IMAGE_MAX_DIM, config.TRAIN_ROIS_PER_IMAGE, config.ROI_POSITIVE_RATIO, stage='test')
+    m.load_weights(config.rcnn_weights, by_name=True)
+    m.summary()
+    # 预测边框、得分、类别
+    predict_boxes = []
+    predict_scores = []
+    predict_labels = []
+    # 逐个图像处理
+    for id in range(len(test_image_info_list)):
+        image, image_meta, _ = image_utils.load_image_gt(id,
+                                                         test_image_info_list[id]['filepath'],
+                                                         config.IMAGE_MAX_DIM,
+                                                         test_image_info_list[id]['boxes'])
+        boxes, scores, class_ids, class_logits = m.predict(
+            [np.expand_dims(image, axis=0), np.expand_dims(image_meta, axis=0)])
+        boxes = np_utils.remove_pad(boxes[0])
+        scores = np_utils.remove_pad(scores[0])[:, 0]
+        class_ids = np_utils.remove_pad(class_ids[0])[:, 0]
+        # 还原检测边框到
+        window = image_meta[7:11]
+        scale = image_meta[11]
+        boxes = image_utils.recover_detect_boxes(boxes, window, scale)
+        # 添加到列表中
+        predict_boxes.append(boxes)
+        predict_scores.append(scores)
+        predict_labels.append(class_ids)
+        if id % 100 == 0:
+            print('预测完成：{}'.format(id + 1))
+
+    # 以下是评估过程
+    annotations = eval_utils.get_annotations(test_image_info_list, config.NUM_CLASSES)
+    detections = eval_utils.get_detections(predict_boxes, predict_scores, predict_labels, config.NUM_CLASSES)
+    average_precisions = eval_utils.voc_eval(annotations, detections, iou_threshold=0.5, use_07_metric=True)
+    print("ap:{}".format(average_precisions))
+    # 求mean ap 去除背景类
+    mAP = np.mean(np.array(list(average_precisions.values()))[1:])
+    print("mAP:{}".format(mAP))
+
+
+if __name__ == '__main__':
+    main()
